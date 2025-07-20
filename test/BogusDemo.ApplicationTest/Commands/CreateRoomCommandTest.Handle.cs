65654aec144bc8335eb5271b4b40bdeb1d3bd217
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-07-20 14:37:53 +0530

    save

---
+++ test/BogusDemo.ApplicationTest/Commands/CreateRoomCommandTest.Handle.cs
@@ -0,0 +1,29 @@
+﻿namespace BogusDemo.ApplicationTest.Commands;
+
+public partial class CreateRoomCommandTest
+{
+    [Fact]
+    public async Task Handler_WorkProperly_GivenValidInput()
+    {
+        // Arrange
+        var department = new Department("Test Department");
+
+        _repoMock
+            .Setup(r => r.GetAsync(1, _ct))
+            .ReturnsAsync(department);
+
+        _repoMock.Setup(r => r.SaveChangesAsync(_ct));
+
+        // Act
+        var isSuccess = await _commandHandler.Handle(_command, _ct);
+
+        // Assert
+        isSuccess.ShouldBeTrue();
+        department.Rooms.Count.ShouldBe(1);
+        department.Rooms[0].RoomNumber.ShouldBe("R001");
+
+        _repoMock.Verify(r => r.GetAsync(1, _ct), Times.Once());
+        _repoMock.Verify(r => r.SaveChangesAsync(_ct), Times.Once());
+        _repoMock.VerifyNoOtherCalls();
+    }
+}
---
+++ test/BogusDemo.ApplicationTest/Commands/CreateRoomCommandTest.cs
@@ -0,0 +1,17 @@
+﻿namespace BogusDemo.ApplicationTest.Commands;
+
+public partial class CreateRoomCommandTest
+{
+    private readonly CreateRoomCommand _command;
+    private readonly IRequestHandler<CreateRoomCommand, bool> _commandHandler;
+    private readonly Mock<IDepartmentRepo> _repoMock;
+    private readonly CancellationToken _ct;
+
+    public CreateRoomCommandTest()
+    {
+        _command = new CreateRoomCommand(1, "R001");
+        _repoMock = new Mock<IDepartmentRepo>();
+        _commandHandler = new CreateRoomCommandHandler(_repoMock.Object);
+        _ct = CancellationToken.None;
+    }
+}
