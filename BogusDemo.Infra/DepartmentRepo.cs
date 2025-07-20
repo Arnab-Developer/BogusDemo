c4673c85f0d774ea656277b8e518c4bfbc91ea98
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-07-20 13:22:01 +0530

    save

---
+++ BogusDemo.Infra/DepartmentRepo.cs
@@ -0,0 +1,45 @@
+﻿using BogusDemo.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace BogusDemo.Infra;
+
+public class DepartmentRepo : IDepartmentRepo
+{
+    private readonly BogusDemoContext _context;
+
+    public DepartmentRepo(BogusDemoContext context)
+    {
+        _context = context;
+    }
+
+    async Task IDepartmentRepo.AddAsync(Department department, CancellationToken ct)
+    {
+        await _context.Departments.AddAsync(department, ct).ConfigureAwait(false);
+    }
+
+    async Task IDepartmentRepo.UpdateAsync(int id, Department department, CancellationToken ct)
+    {
+        await Task.CompletedTask;
+    }
+
+    async Task IDepartmentRepo.DeleteAsync(int id, CancellationToken ct)
+    {
+        var department = await _context.Departments.FirstAsync(d => d.Id == id, ct)
+            .ConfigureAwait(false);
+
+        _context.Departments.Remove(department);
+    }
+
+    async public Task<Department> Get(int id, CancellationToken ct)
+    {
+        var department = await _context.Departments.FirstAsync(d => d.Id == id, ct)
+            .ConfigureAwait(false);
+
+        return department;
+    }
+
+    async Task IDepartmentRepo.SaveChangesAsunc(CancellationToken ct)
+    {
+        await _context.SaveChangesAsync(ct).ConfigureAwait(false);
+    }
+}
---
+++ BogusDemo.Infra/IDepartmentRepo.cs
@@ -0,0 +1,37 @@
+﻿using BogusDemo.Core;
+
+namespace BogusDemo.Infra;
+
+/// <summary>Repo class of department.</summary>
+public interface IDepartmentRepo
+{
+    /// <summary>Add new department.</summary>
+    /// <param name="department">The new department.</param>
+    /// <param name="ct">The cancellation token to cancel the async work.</param>
+    /// <returns>A task.</returns>
+    public Task AddAsync(Department department, CancellationToken ct);
+
+    /// <summary>Update the department.</summary>
+    /// <param name="id">The id of the existing department.</param>
+    /// <param name="department">The new department.</param>
+    /// <param name="ct">The cancellation token to cancel the async work.</param>
+    /// <returns>A task.</returns>
+    public Task UpdateAsync(int id, Department department, CancellationToken ct);
+
+    /// <summary>Delete the department.</summary>
+    /// <param name="id">The id of the existing department.</param>
+    /// <param name="ct">The cancellation token to cancel the async work.</param>
+    /// <returns>A task.</returns>
+    public Task DeleteAsync(int id, CancellationToken ct);
+
+    /// <summary>Gets a department by id.</summary>
+    /// <param name="id">The id of the existing department.</param>
+    /// <param name="ct">The cancellation token to cancel the async work.</param>
+    /// <returns>A task containing a department object.</returns>
+    public Task<Department> Get(int id, CancellationToken ct);
+
+    /// <summary>Save the changes in the datastore.</summary>
+    /// <param name="ct">The cancellation token to cancel the async work.</param>
+    /// <returns>A task.</returns>
+    public Task SaveChangesAsunc(CancellationToken ct);
+}
