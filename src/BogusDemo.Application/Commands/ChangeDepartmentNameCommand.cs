0350f6555b894ef671211305cb7f1ac99555cbbf
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-07-20 12:36:42 +0530

    save

---
+++ src/BogusDemo.Application/Commands/ChangeDepartmentNameCommand.cs
@@ -0,0 +1,26 @@
+﻿using BogusDemo.Infra;
+using MediatR;
+
+namespace BogusDemo.Application.Commands;
+
+public record ChangeDepartmentNameCommand(int Id, string Name) : IRequest<bool>;
+
+public class ChangeDepartmentNameCommandHandler : IRequestHandler<ChangeDepartmentNameCommand, bool>
+{
+    private readonly BogusDemoContext _context;
+
+    public ChangeDepartmentNameCommandHandler(BogusDemoContext context)
+    {
+        _context = context;
+    }
+
+    async Task<bool> IRequestHandler<ChangeDepartmentNameCommand, bool>.Handle(
+        ChangeDepartmentNameCommand request,
+        CancellationToken ct)
+    {
+        var department = _context.Departments.First(d => d.Id == request.Id);
+        department.ChangeName(request.Name);
+        await _context.SaveChangesAsync(ct).ConfigureAwait(false);
+        return true;
+    }
+}
---
+++ src/BogusDemo.Application/Commands/CreateDepartmentCommand.cs
@@ -0,0 +1,27 @@
+﻿using BogusDemo.Core;
+using BogusDemo.Infra;
+using MediatR;
+
+namespace BogusDemo.Application.Commands;
+
+public record CreateDepartmentCommand(string Name) : IRequest<bool>;
+
+public class CreateDepartmentCommandHandler : IRequestHandler<CreateDepartmentCommand, bool>
+{
+    private readonly BogusDemoContext _context;
+
+    public CreateDepartmentCommandHandler(BogusDemoContext context)
+    {
+        _context = context;
+    }
+
+    async Task<bool> IRequestHandler<CreateDepartmentCommand, bool>.Handle(
+        CreateDepartmentCommand request,
+        CancellationToken ct)
+    {
+        var department = new Department(request.Name);
+        await _context.Departments.AddAsync(department, ct).ConfigureAwait(false);
+        await _context.SaveChangesAsync(ct).ConfigureAwait(false);
+        return true;
+    }
+}
