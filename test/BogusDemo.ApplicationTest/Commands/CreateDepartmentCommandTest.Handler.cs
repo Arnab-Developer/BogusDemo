ac0de127407bf5e74b21581d16e077fcab509264
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-07-20 14:14:45 +0530

    save

---
+++ test/BogusDemo.ApplicationTest/Commands/CreateDepartmentCommandTest.Handler.cs
@@ -0,0 +1,27 @@
+﻿using BogusDemo.Core;
+using Shouldly;
+
+namespace BogusDemo.ApplicationTest.Commands;
+
+public partial class CreateDepartmentCommandTest
+{
+    [Fact]
+    public async Task Handler_WorkProperly_GivenValidInput()
+    {
+        // Arrange
+        var department = new Department("Test Department");
+
+        _repoMock.Setup(r => r.AddAsync(It.IsAny<Department>(), _ct));
+        _repoMock.Setup(r => r.SaveChangesAsync(_ct));
+
+        // Act
+        var isSuccess = await _commandHandler.Handle(_command, _ct);
+
+        // Assert
+        isSuccess.ShouldBeTrue();
+
+        _repoMock.Verify(r => r.AddAsync(It.IsAny<Department>(), _ct), Times.Once());
+        _repoMock.Verify(r => r.SaveChangesAsync(_ct), Times.Once());
+        _repoMock.VerifyNoOtherCalls();
+    }
+}
---
+++ test/BogusDemo.ApplicationTest/Commands/CreateDepartmentCommandTest.cs
@@ -0,0 +1,21 @@
+﻿using BogusDemo.Application.Commands;
+using BogusDemo.Infra;
+using MediatR;
+
+namespace BogusDemo.ApplicationTest.Commands;
+
+public partial class CreateDepartmentCommandTest
+{
+    private readonly CreateDepartmentCommand _command;
+    private readonly IRequestHandler<CreateDepartmentCommand, bool> _commandHandler;
+    private readonly Mock<IDepartmentRepo> _repoMock;
+    private readonly CancellationToken _ct;
+
+    public CreateDepartmentCommandTest()
+    {
+        _command = new CreateDepartmentCommand("Test Department");
+        _repoMock = new Mock<IDepartmentRepo>();
+        _commandHandler = new CreateDepartmentCommandHandler(_repoMock.Object);
+        _ct = CancellationToken.None;
+    }
+}
