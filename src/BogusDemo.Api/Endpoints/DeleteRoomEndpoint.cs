bbf34250853e2c0979d7693831c7112a1cabb5d9
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-07-21 09:03:15 +0530

    endpoint

---
+++ src/BogusDemo.Api/Endpoints/DeleteRoomEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using BogusDemo.Application.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace BogusDemo.Api.Endpoints;
+
+internal static class DeleteRoomEndpoint
+{
+    public static void MapDeleteRoomEndpoint(this IEndpointRouteBuilder builder)
+    {
+        builder.MapDelete("delete-room", HandleAsync);
+    }
+
+    private static async Task<Results<Ok, NotFound>> HandleAsync(
+        int departmentId, int roomId, IMediator mediator, CancellationToken ct)
+    {
+        var command = new DeleteRoomCommand(departmentId, roomId);
+        var isSuccess = await mediator.Send(command, ct);
+
+        return isSuccess ? TypedResults.Ok() : TypedResults.NotFound();
+    }
+}
