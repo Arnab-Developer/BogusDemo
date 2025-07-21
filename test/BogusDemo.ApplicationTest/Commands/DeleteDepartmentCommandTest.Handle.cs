34fb26b5805a71f6c9bbca3758c3ea830ceb9dfb
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-07-21 10:42:42 +0530

    save

---
+++ test/BogusDemo.ApplicationTest/Commands/DeleteDepartmentCommandTest.Handle.cs
@@ -0,0 +1,25 @@
+﻿namespace BogusDemo.ApplicationTest.Commands;
+
+public partial class DeleteDepartmentCommandTest
+{
+    [Fact]
+    public async Task Handler_WorkProperly_GivenValidInput()
+    {
+        // Arrange
+        _command = new DeleteDepartmentCommand(1);
+        var department = new Department("Test Department");
+
+        _repoMock.Setup(r => r.DeleteAsync(1, _ct));
+        _repoMock.Setup(r => r.SaveChangesAsync(_ct));
+
+        // Act
+        var isSuccess = await _commandHandler.Handle(_command, _ct);
+
+        // Assert
+        isSuccess.ShouldBeTrue();
+
+        _repoMock.Verify(r => r.DeleteAsync(1, _ct), Times.Once());
+        _repoMock.Verify(r => r.SaveChangesAsync(_ct), Times.Once());
+        _repoMock.VerifyNoOtherCalls();
+    }
+}
---
+++ test/BogusDemo.ApplicationTest/Commands/DeleteDepartmentCommandTest.cs
@@ -0,0 +1,16 @@
+﻿namespace BogusDemo.ApplicationTest.Commands;
+
+public partial class DeleteDepartmentCommandTest
+{
+    private DeleteDepartmentCommand? _command;
+    private readonly IRequestHandler<DeleteDepartmentCommand, bool> _commandHandler;
+    private readonly Mock<IDepartmentRepo> _repoMock;
+    private readonly CancellationToken _ct;
+
+    public DeleteDepartmentCommandTest()
+    {
+        _repoMock = new Mock<IDepartmentRepo>();
+        _commandHandler = new DeleteDepartmentCommandHandler(_repoMock.Object);
+        _ct = CancellationToken.None;
+    }
+}
