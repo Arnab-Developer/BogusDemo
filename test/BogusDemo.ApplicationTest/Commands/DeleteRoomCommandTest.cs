2c3c396b64d66aa89f0bcd4cb1d3b16a92dfb605
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-07-21 08:59:29 +0530

    save

---
+++ test/BogusDemo.ApplicationTest/Commands/DeleteRoomCommandTest.cs
@@ -0,0 +1,16 @@
+﻿namespace BogusDemo.ApplicationTest.Commands;
+
+public partial class DeleteRoomCommandTest
+{
+    private DeleteRoomCommand? _command;
+    private readonly IRequestHandler<DeleteRoomCommand, bool> _commandHandler;
+    private readonly Mock<IDepartmentRepo> _repoMock;
+    private readonly CancellationToken _ct;
+
+    public DeleteRoomCommandTest()
+    {
+        _repoMock = new Mock<IDepartmentRepo>();
+        _commandHandler = new DeleteRoomCommandHandler(_repoMock.Object);
+        _ct = CancellationToken.None;
+    }
+}
