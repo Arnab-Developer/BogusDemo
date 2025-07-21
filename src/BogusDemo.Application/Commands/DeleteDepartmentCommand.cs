3b96cd1cdca6877d5471aeabd01a20c8c6fc5608
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-07-21 10:36:30 +0530

    save

---
+++ src/BogusDemo.Application/Commands/DeleteDepartmentCommand.cs
@@ -0,0 +1,24 @@
+﻿using BogusDemo.Infra;
+using MediatR;
+
+namespace BogusDemo.Application.Commands;
+
+public record DeleteDepartmentCommand(int DepartmentId) : IRequest<bool>;
+
+public class DeleteDepartmentCommandHandler : IRequestHandler<DeleteDepartmentCommand, bool>
+{
+    private readonly IDepartmentRepo _departmentRepo;
+
+    public DeleteDepartmentCommandHandler(IDepartmentRepo departmentRepo)
+    {
+        _departmentRepo = departmentRepo;
+    }
+
+    async Task<bool> IRequestHandler<DeleteDepartmentCommand, bool>.Handle(
+        DeleteDepartmentCommand request, CancellationToken ct)
+    {
+        await _departmentRepo.DeleteAsync(request.DepartmentId, ct).ConfigureAwait(false);
+        await _departmentRepo.SaveChangesAsync(ct).ConfigureAwait(false);
+        return true;
+    }
+}
