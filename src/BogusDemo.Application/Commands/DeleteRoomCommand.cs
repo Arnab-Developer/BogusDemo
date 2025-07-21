48dcca1a9c95144b928bdc67206cbd33f57b3019
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-07-21 08:43:26 +0530

    save

---
+++ src/BogusDemo.Application/Commands/DeleteRoomCommand.cs
@@ -0,0 +1,27 @@
+﻿using BogusDemo.Infra;
+using MediatR;
+
+namespace BogusDemo.Application.Commands;
+
+public record DeleteRoomCommand(int DepartmentId, int RoomId) : IRequest<bool>;
+
+public class DeleteRoomCommandHandler : IRequestHandler<DeleteRoomCommand, bool>
+{
+    private readonly IDepartmentRepo _departmentRepo;
+
+    public DeleteRoomCommandHandler(IDepartmentRepo departmentRepo)
+    {
+        _departmentRepo = departmentRepo;
+    }
+
+    async Task<bool> IRequestHandler<DeleteRoomCommand, bool>.Handle(
+        DeleteRoomCommand request, CancellationToken ct)
+    {
+        var department = await _departmentRepo.GetAsync(request.DepartmentId, ct)
+            .ConfigureAwait(false);
+
+        department.DeleteRoom(request.RoomId);
+        await _departmentRepo.SaveChangesAsync(ct).ConfigureAwait(false);
+        return true;
+    }
+}
