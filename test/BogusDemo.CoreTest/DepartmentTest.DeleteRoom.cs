32f9b05ad102b00e45c43a718cc30ac40464f767
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-07-21 08:48:45 +0530

    core test

---
+++ test/BogusDemo.CoreTest/DepartmentTest.DeleteRoom.cs
@@ -0,0 +1,18 @@
+﻿namespace BogusDemo.CoreTest;
+
+public partial class DepartmentTest
+{
+    [Fact]
+    public void DeleteRoom_WorkProperly_GivenValidInput()
+    {
+        // Arrange
+        var inputName = new Faker().Random.String2(10);
+        _department.CreateRoom(inputName);
+
+        // Act
+        _department.DeleteRoom(0);
+
+        // Assert
+        _department.Rooms.ShouldBeEmpty();
+    }
+}
