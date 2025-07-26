b23d18585e988a211b67e5a9bc2b48e0343d4457
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-07-26 13:41:25 +0530

    Get department data query test (#16)

---
+++ test/BogusDemo.ApplicationTest/Queries/GetDepartmentsQueryTest.Handle.cs
@@ -0,0 +1,64 @@
+﻿namespace BogusDemo.ApplicationTest.Queries;
+
+public partial class GetDepartmentsQueryTest
+{
+    [Theory]
+    [InlineData(1, "d1", "r1", "d2", "r2")]
+    [InlineData(2, "d3", "r3", "d4", "r4")]
+    public async Task Handle_ReturnProperData_GivenPartialDataPageSize(
+        int pageNumber, params string[] data)
+    {
+        // Arrange
+        _query = new GetDepartmentsQuery(pageNumber, 2);
+
+        // Act
+        var departmentDTOs = await _queryHandler.Handle(_query, _ct);
+
+        // Assert
+        departmentDTOs.Count().ShouldBe(2);
+
+        departmentDTOs.ElementAt(0).Name.ShouldBe(data[0]);
+        departmentDTOs.ElementAt(0).Rooms.ElementAt(0).RoomNumber.ShouldBe(data[1]);
+
+        departmentDTOs.ElementAt(1).Name.ShouldBe(data[2]);
+        departmentDTOs.ElementAt(1).Rooms.ElementAt(0).RoomNumber.ShouldBe(data[3]);
+    }
+
+    [Fact]
+    public async Task Handle_ReturnProperData_GivenFullDataPageSize()
+    {
+        // Arrange
+        _query = new GetDepartmentsQuery(1, 4);
+
+        // Act
+        var departmentDTOs = await _queryHandler.Handle(_query, _ct);
+
+        // Assert
+        departmentDTOs.Count().ShouldBe(4);
+
+        departmentDTOs.ElementAt(0).Name.ShouldBe("d1");
+        departmentDTOs.ElementAt(0).Rooms.ElementAt(0).RoomNumber.ShouldBe("r1");
+
+        departmentDTOs.ElementAt(1).Name.ShouldBe("d2");
+        departmentDTOs.ElementAt(1).Rooms.ElementAt(0).RoomNumber.ShouldBe("r2");
+
+        departmentDTOs.ElementAt(2).Name.ShouldBe("d3");
+        departmentDTOs.ElementAt(2).Rooms.ElementAt(0).RoomNumber.ShouldBe("r3");
+
+        departmentDTOs.ElementAt(3).Name.ShouldBe("d4");
+        departmentDTOs.ElementAt(3).Rooms.ElementAt(0).RoomNumber.ShouldBe("r4");
+    }
+
+    [Fact]
+    public async Task Handle_ReturnEmptyData_GivenInvalidPageNumber()
+    {
+        // Arrange
+        _query = new GetDepartmentsQuery(2, 4);
+
+        // Act
+        var departmentDTOs = await _queryHandler.Handle(_query, _ct);
+
+        // Assert
+        departmentDTOs.ShouldBeEmpty();
+    }
+}
---
+++ test/BogusDemo.ApplicationTest/Queries/GetDepartmentsQueryTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BogusDemo.ApplicationTest.Queries;
+
+public partial class GetDepartmentsQueryTest
+{
+    private GetDepartmentsQuery? _query;
+    private readonly IRequestHandler<GetDepartmentsQuery, IEnumerable<DepartmentDTO>> _queryHandler;
+    private readonly CancellationToken _ct;
+
+    public GetDepartmentsQueryTest()
+    {
+        var context = new BogusDemoContext(new DbContextOptionsBuilder<BogusDemoContext>()
+            .UseInMemoryDatabase("testdb")
+            .Options);
+
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+        SeedData(context);
+
+        _queryHandler = new GetDepartmentsQueryHandler(context);
+        _ct = new CancellationToken();
+    }
+
+    private static void SeedData(BogusDemoContext context)
+    {
+        var department1 = new Department("d1");
+        department1.CreateRoom("r1");
+
+        var department2 = new Department("d2");
+        department2.CreateRoom("r2");
+
+        var department3 = new Department("d3");
+        department3.CreateRoom("r3");
+
+        var department4 = new Department("d4");
+        department4.CreateRoom("r4");
+
+        var departments = new List<Department>()
+        {
+            department1,
+            department2,
+            department3,
+            department4
+        };
+
+        context.Departments.AddRange(departments);
+        context.SaveChanges();
+    }
+}
